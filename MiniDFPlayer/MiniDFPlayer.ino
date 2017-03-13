0fb3d73616371576c54b444c79a5d6a6e19e7d81
Unknown <fatte@hotmail.it>
2017-03-13 14:04:12 +0100

    Software to test the MiniDFPlayer

---
+++ MiniDFPlayer/MiniDFPlayer.ino
@@ -0,0 +1,21 @@
+
+#include <SoftwareSerial.h>
+#include <DFPlayer_Mini_Mp3.h>
+
+void setup () {
+ Serial.begin (9600);
+ mp3_set_serial (Serial); //set Serial for DFPlayer-mini mp3 module 
+ mp3_set_volume (30); //max volume is 30 (not really sure)
+}
+ 
+void loop () {        
+
+ mp3_play (1); //play 0001.mp3
+ delay (30000); //30 sec, time delay to allow 0001.mp3 to finish playing
+
+ mp3_play (2); //play 0002.mp3
+ delay (20000); 
+
+ mp3_pause (); //pause the mp3
+
+}
