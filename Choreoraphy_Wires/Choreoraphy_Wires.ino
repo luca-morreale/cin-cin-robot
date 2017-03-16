3c740434b2221010e267f6c18e4958b2a648aa7f
Luca Morreale <luca.morreale93@gmail.com>
2017-03-16 17:33:25 +0100

    Updated choreography

--- Choreoraphy_Wires/Choreoraphy_Wires.ino
+++ Choreoraphy_Wires/Choreoraphy_Wires.ino
@@ -4,50 +4,49 @@
 
 #define LEFT_PIN 9
 #define RIGHT_PIN 10
+#define BOW_PIN 11
 
-#define LEFT_MIDDLE_POSITION 180
-#define LEFT_STRETCHED_POSITION 0
-#define LEFT_REST_POSITION 270
+#define LEFT_STRETCHED_POSITION 10
+#define LEFT_REST_POSITION 180
 
-#define RIGHT_MIDDLE_POSITION 180
-#define RIGHT_STRETCHED_POSITION 0
-#define RIGHT_REST_POSITION 270
+#define RIGHT_STRETCHED_POSITION 10
+#define RIGHT_REST_POSITION 180
+
+#define BOW_STRETCHED_POSITION 160
+#define BOW_REST_POSITION 90
 
 Servo leftMotor, rightMotor, bowMotor;
 
 void setup() {
-    leftMotor.attach(9);
-    rightMotor.attach(10);
-    bowMotor.attach(11);
+    leftMotor.attach(LEFT_PIN);
+    rightMotor.attach(RIGHT_PIN);
+    bowMotor.attach(BOW_PIN);
     
-    leftMotor.write(LEFT_MIDDLE_POSITION);
-    rightMotor.write(RIGHT_MIDDLE_POSITION);
-    
-    bowMotor.write(5);
-    
+    leftMotor.write(LEFT_REST_POSITION);
+    rightMotor.write(RIGHT_REST_POSITION);
+    bowMotor.write(BOW_REST_POSITION);   
 }
 
 
 void loop() {
     
     leftMotor.write(LEFT_STRETCHED_POSITION);
-    delay(1200);
+    delay(1000);
     leftMotor.write(LEFT_REST_POSITION);
-    delay(1200);
+    delay(1000);
     
     rightMotor.write(RIGHT_STRETCHED_POSITION);
-    delay(1500);
+    delay(1000);
     rightMotor.write(RIGHT_REST_POSITION);
-    delay(1500);
+    delay(1000);
     
-    bowMotor.write(170);
-    delay(1500);
-    bowMotor.write(5);
+    bowMotor.write(BOW_STRETCHED_POSITION);
+    delay(5000);
+    bowMotor.write(BOW_REST_POSITION);
     delay(1500);
         
     delay(1000);
 }
-
 
 
 
