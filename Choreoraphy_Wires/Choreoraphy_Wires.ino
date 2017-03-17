57f54590dc2392ff3e9cb89f3b1e255558b6981c
Unknown <fatte@hotmail.it>
2017-03-17 18:03:47 +0100

    Updated choreography

--- Choreoraphy_Wires/Choreoraphy_Wires.ino
+++ Choreoraphy_Wires/Choreoraphy_Wires.ino
@@ -5,47 +5,78 @@
 #define LEFT_PIN 9
 #define RIGHT_PIN 10
 #define BOW_PIN 11
+#define RIGHT_ARM 12
 
-#define LEFT_STRETCHED_POSITION 10
+#define LEFT_STRETCHED_POSITION 30
 #define LEFT_REST_POSITION 180
 
-#define RIGHT_STRETCHED_POSITION 10
+#define RIGHT_STRETCHED_POSITION 30
 #define RIGHT_REST_POSITION 180
 
 #define BOW_STRETCHED_POSITION 160
 #define BOW_REST_POSITION 90
 
-Servo leftMotor, rightMotor, bowMotor;
+#define RARM_HIGH_POSITION 0
+#define RARM_LOW_POSITION 90
+
+#define tempo 7
+
+Servo leftMotor, rightMotor, bowMotor, rarmMotor;
 
 void setup() {
     leftMotor.attach(LEFT_PIN);
     rightMotor.attach(RIGHT_PIN);
     bowMotor.attach(BOW_PIN);
+    rarmMotor.attach(RIGHT_ARM);
     
     leftMotor.write(LEFT_REST_POSITION);
     rightMotor.write(RIGHT_REST_POSITION);
     bowMotor.write(BOW_REST_POSITION);   
+    rarmMotor.write(LEFT_REST_POSITION);
 }
 
 
 void loop() {
+
     
-    leftMotor.write(LEFT_STRETCHED_POSITION);
-    delay(1000);
-    leftMotor.write(LEFT_REST_POSITION);
-    delay(1000);
+    for(int i=LEFT_REST_POSITION;i>LEFT_STRETCHED_POSITION;i--){
+    leftMotor.write(i);
     
-    rightMotor.write(RIGHT_STRETCHED_POSITION);
-    delay(1000);
-    rightMotor.write(RIGHT_REST_POSITION);
-    delay(1000);
     
-    bowMotor.write(BOW_STRETCHED_POSITION);
-    delay(5000);
-    bowMotor.write(BOW_REST_POSITION);
-    delay(1500);
+    delay(tempo);
+    }
+   
+    for(int i=LEFT_STRETCHED_POSITION;i<LEFT_REST_POSITION;i++){
+    leftMotor.write(i);
+
+    delay(tempo);
+    }
+   // delay(1000);
+    
+    for(int i=RIGHT_REST_POSITION;i>RIGHT_STRETCHED_POSITION;i--){
+    rightMotor.write(i);
+    rarmMotor.write(i/2.5);
+    delay(tempo);
+    }
+    for(int i=RIGHT_STRETCHED_POSITION;i<RIGHT_REST_POSITION;i++){
+    rightMotor.write(i);
+    rarmMotor.write(i/2.5);
+    delay(tempo);
+    }
+   // delay(1000);
+
+    for(int i=BOW_REST_POSITION;i<BOW_STRETCHED_POSITION;i++){
+    bowMotor.write(i);
+    delay(15);
+    }
+    delay(2500);
+    for(int i=BOW_STRETCHED_POSITION;i>BOW_REST_POSITION;i--){
+    bowMotor.write(i);
+    delay(10);
+    }
+    
         
-    delay(1000);
+//    delay(1000);
 }
 
 
