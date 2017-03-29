f510fb76db6df61f8396e832ea05a467da34090c
Unknown <fatte@hotmail.it>
2017-03-29 11:37:47 +0200

    Infrared Sensor Software

---
+++ IR_Sensor/IR_Sensor.ino
@@ -0,0 +1,15 @@
+#define sensor A0 // Sharp IR (10-80 cm)
+
+void setup() {
+  Serial.begin(9600); // start the serial port
+}
+
+void loop() {
+  
+  // 5v
+  float volts = analogRead(sensor);  // value from sensor * (5/1024)
+  int distance = (6762/(volts-9))-4; // worked out from datasheet graph
+  delay(1000); // slow down serial port 
+  
+    Serial.println(distance);   // print the distance
+}
